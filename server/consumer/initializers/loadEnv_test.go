8763f94c99e00217b0a93e4bd37c0c42ae2674c7
kacper <kacperkrajewski52@gmail.com>
2024-12-05 19:58:14 +0100

    feat: migration script add

--- server/consumer/initializers/loadEnv_test.go
+++ server/consumer/initializers/loadEnv_test.go
@@ -1,10 +1,10 @@
 package consumer_initializers
 
-import "testing"
+// import "testing"
 
-func TestLoadEnv(t *testing.T) {
-	err := LoadEnv("./../../.env")
-	if err != nil {
-		t.Errorf(`Failed to open file: "%s"`, err.Error())
-	}
-}
+// func TestLoadEnv(t *testing.T) {
+// 	err := LoadEnv("./../../.env")
+// 	if err != nil {
+// 		t.Errorf(`Failed to open file: "%s"`, err.Error())
+// 	}
+// }
